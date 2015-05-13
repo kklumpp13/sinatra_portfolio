8c0e055549628dd238101d8f570109f6d8bc7694
Krystal Klumpp <kklumpp13@gmail.com>
2015-05-13 13:31:42 -0400

    Initial commit

---
+++ public/portfolio.js
@@ -0,0 +1,65 @@
+$(function() {
+
+  "use strict";
+
+  var topoffset = 50; //variable for menu height
+  var slideqty = $('#featured .item').length;
+  var wheight = $(window).height(); //get the height of the window
+  var randSlide = Math.floor(Math.random()*slideqty);
+
+  $('#featured .item').eq(randSlide).addClass('active');
+
+
+  $('.fullheight').css('height', weight); //set to window tallness  
+
+
+  //Activate Scrollspy
+  $('body').scrollspy({
+    target: 'header .navbar',
+    offset: topoffset
+  });
+
+  // add inbody class
+  var hash = $(this).find('li.active a').attr('href');
+  if(hash !== '#featured') {
+    $('header nav').addClass('inbody');
+  } else {
+    $('header nav').removeClass('inbody');
+  }
+
+
+  // Add an inbody class to nav when scrollspy event fires
+  $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+    var hash = $(this).find('li.active a').attr('href');
+    if(hash !== '#featured') {
+      $('header nav').addClass('inbody');
+    } else {
+      $('header nav').removeClass('inbody');
+    }
+  });
+
+  
+  //$(".nav a").on("click", function(){
+   //$(".nav").find(".active").removeClass("active");
+   //$(this).parent().addClass("active");
+//});
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+
+  
+
+});
